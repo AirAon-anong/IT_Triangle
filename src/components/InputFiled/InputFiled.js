0214c495f2ba2eec3bf7424f22165766eb5dbe74
Aon-anong Panplang <panplang4@gmail.com>
2022-02-20 17:25:37 +0700

    pull3

--- src/components/InputFiled/InputFiled.js
+++ src/components/InputFiled/InputFiled.js
@@ -1,18 +1,16 @@
-import {React, useState } from 'react'
+import { React, useState } from "react";
 
 export default function InputFiled(props) {
-    const [side1, setSide1] = useState("");
+  const [side1, setSide1] = useState("");
 
   return (
-    <form>
-        <label>
-          {props.name}
-          <input
-            type="double"
-            value={side1}
-            onChange={(e) => setSide1(e.target.value)}
-          />
-        </label>
-      </form>
-  )
+    <div className="inputField">
+      {props.name}
+        <input
+          type="double"
+          value={side1}
+          onChange={(e) => setSide1(e.target.value)}
+        />
+    </div>
+  );
 }
