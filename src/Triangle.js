7a2108de31f629be3b59a2b203420072045c16d1
GoldF15h <todsapon.singsunjit@gmail.com>
2022-02-26 18:37:25 +0700

    add modal

--- src/Triangle.js
+++ src/Triangle.js
@@ -1,11 +1,19 @@
-import React, { useReducer } from "react";
+import { React } from "react";
 import UserInterface from "./components/UserInterface/UserInterface";
+import PopupModal from "./components/PopupModal/PopupModal";
+import BackDrop from "./components/PopupModal/BackDrop";
 import "./Triangle.css";
+import { useState } from "react";
 
 export default function Triangle() {
+  const [modalIsOpen, setmodalIsOpen] = useState(false);
   return (
-    <div className="All">
-      <UserInterface></UserInterface>
+    <div>
+      <div className="All">
+        <UserInterface></UserInterface>
+      </div>
+      {modalIsOpen ? <PopupModal></PopupModal> : <div></div>}
+      {modalIsOpen ? <BackDrop></BackDrop> : <div></div>}
     </div>
   );
 }
---
+++ src/components/PopupModal/PopupModal.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./PopupModal.css";
+
+export default function PopupModal() {
+  return <div className="modal">PopupModal</div>;
+}
